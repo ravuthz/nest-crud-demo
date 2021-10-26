db56a3fc83b18580ea6602a21e9406d872593872
ravuthz <ravuthz@gmail.com>
2021-10-26 22:55:30 +0700

    Add swagger and swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJs CRUD')
+    .setDescription('NestJs CRUD Application')
+    .setVersion('1.0.0')
+    .addTag('nestjs')
+    .build();
+
+  const documents = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('docs', app, documents);
+
   await app.listen(3000);
 }
 bootstrap();
